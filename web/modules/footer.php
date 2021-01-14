45b7eea548a703cdcbeab963d2fd44236312d8b9
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-01-14 11:43:25 -0500

    mix footer

--- web/modules/footer.php
+++ web/modules/footer.php
@@ -3,6 +3,6 @@
         <a href="https://www.facebook.com/jeremiah.astin" target="_blank" class="social"><i class="fab fa-facebook-f"></i></a>
         <a href="https://twitter.com/astin_jeremiah?p=s" target="_blank" class="social"><i class="fab fa-twitter"></i></a>
         <a href="https://www.instagram.com/astinjeremy/" target="_blank"><i class="fab fa-instagram"></i></a>
-        <p>@2021 Jeremiah Astin</p>
+        <p class="mb-1">@2021 Jeremiah Astin</p>
     </div>
 </div>
