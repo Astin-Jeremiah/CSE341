7bd28ae56c6e0738b49c99f581094881f5ab4f73
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-01-14 10:37:40 -0500

    test php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,30 @@
-<?php
+<!DOCTYPE HTML>
+<html lang="en-us" class="h-100">
 
-  phpinfo();
+<head>
+    <meta charset="utf-8">
+    <title>CSE 341 Webpage</title>
+    <link href="bootstrap.min.css" type="text/css" rel="stylesheet" />
+    <link href="style.css" type="text/css" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/4e98ed47d3.js" crossorigin="anonymous"></script>
+</head>
 
-?>
+<body class="d-flex flex-column h-100">
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
+    </header>
+
+    <main class="flex-shrink-0">
+        <div class="container text-center">
+            <h1 class="m-4 text-dark">Jeremiah Astin <br>CSE 341 Portfolio</h1>
+            <img class="img-fluid" src="images/family.jpg" alt="Astin Family Photo" />
+            <blockquote class="text-danger">“Today is your day!<br>Your mountain is waiting.<br>So...get on your way!<br>Dr.Seuss</blockquote>
+        </div>
+    </main>
+    <footer class="footer mt-auto py-2 bg-dark">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'; ?>
+    </footer>
+
+</body>
+
+</html>
---
+++ web/modules/header.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-md fixed-top bg-danger" id="mainnav">
+    <div class="container">
+        <div class="navbar-collapse justify-content-center">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link fs-3 fw-bold text-dark" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fs-3 fw-bold text-dark" href="about.html">About Me</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fs-3 fw-bold text-dark" href="portfolio.html">Portfolio</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
