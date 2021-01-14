ad44916959de32a96969e3e8b89223b9241dce2b
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-01-14 12:25:50 -0500

    fix footer

--- web/index.php
+++ web/index.php
@@ -21,7 +21,7 @@
             <blockquote class="text-danger">“You're off to great palces!<br>Today is your day!<br>Your mountain is waiting.<br>So...get on your way!"<br><span class="text-primary">Dr.Seuss</span></blockquote>
         </div>
     </main>
-    <footer>
+    <footer class="mt-1 footer mt-auto py-2 bg-primary">
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'; ?>
     </footer>
 
