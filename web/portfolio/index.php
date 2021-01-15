102a89fd17087228e29f7074e3e88b5525c489b0
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-01-14 20:51:03 -0500

    1-14-21

--- web/portfolio/index.php
+++ web/portfolio/index.php
@@ -13,130 +13,29 @@
     <header>
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
     </header>
-
     <main class="flex-shrink-0">
         <div class="container text-center">
             <h1 class="m-4 text-dark">CSE 341 Portfolio</h1>
-            <ol>
-                <li>
-                    <details>
-                        <summary>Week 1</summary>
-                            <ul>
-                                <li><a href="../week1/hello.html">Hello World</a></li>
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 2</summary>
-                            <ul>
-                                <li><a href="../week2/02TeamActicity.html">Week 2 Team Activity</a></li>
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 3</summary>
-                            <ul>
-                                
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 4</summary>
-                            <ul>
-                                
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 5</summary>
-                            <ul>
-                             
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 6</summary>
-                            <ul>
-
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 7</summary>
-                            <ul>
-
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 8</summary>
-                            <ul>
-
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 9</summary>
-                            <ul>
-                                
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 10</summary>
-                            <ul>
-                            
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 11</summary>
-                            <ul>
-                                
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 12</summary>
-                            <ul>
-                               
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 13</summary>
-                            <ul>
-                                
-                            </ul>
-                    </details>
-                </li>
-                <li>
-                    <details>
-                        <summary>Week 14</summary>
-                            <ul>
-                                
-                            </ul>
-                    </details>
-                </li>
-            </ol>
+            <div class="btn-group gap-5">
+                <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Week 1
+                </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="../week1/hello.html">Hello World</a></li>
+                    </ul>
+                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Week 2
+                </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="../week2/02TeamActicity.html">Week 2 Team Activity</a></li>
+                    </ul>
+            </div>
         </div>
     </main>
     <footer class="footer mt-auto py-2 bg-dark">
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'; ?>
     </footer>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 
 </html>
