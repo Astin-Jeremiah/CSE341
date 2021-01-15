869adbac99b81b22dbc30cf4f1c63c3954817cd5
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-01-15 15:51:45 -0500

    fix typo

--- web/about/index.php
+++ web/about/index.php
@@ -20,7 +20,7 @@
             <div class="clearfix">
                 <img class="mb-2" src="../images/jeremiah.jpg" alt="Jeremiah Astin" />
                 <p>Hello! My name is Jeremiah Astin. I am currently an applied technologies student at Brigham Young University Idaho Online. I have always been interested in computers and the internet and I have really enjoyed learning how to design and develop websites.</p>
-                <p>I was born in Anchorage, Alaska, but my father was in the Airforce so we moved all around the country. I currently reside in Dayton, Ohio where I have lived for the last 15 years. I am married to my lovely wife Ashley and we have a baby girl named Karleigh and pit bull named Emma. In 2010 I graduated with a bachelor’s degree in Marketing from Wright State University. I currently work as the Director of Ticketing <a href="https://www.daytonlive.org/" target="new">Dayton Live</a>, a local performing arts organization.</p>
+                <p>I was born in Anchorage, Alaska, but my father was in the Airforce so we moved all around the country. I currently reside in Dayton, Ohio where I have lived for the last 15 years. I am married to my lovely wife Ashley and we have a baby girl named Karleigh and pit bull named Emma. In 2010 I graduated with a bachelor’s degree in Marketing from Wright State University. I currently work as the Director of Ticketing for <a href="https://www.daytonlive.org/" target="new">Dayton Live</a>, a local performing arts organization.</p>
                 <p>In my spare time I enjoy reading, watching movies, and spending time with my family. I also enjoy traveling and visiting amusement parks.</p>
             </div>
         </div>
