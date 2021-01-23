57b2e188320a1b572d0b0db4139483d5a83eb40a
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-01-23 15:05:08 -0500

    fix img tags

--- web/week3/shopping/items.php
+++ web/week3/shopping/items.php
@@ -41,7 +41,7 @@ if (empty($_SESSION['cart'])) {
         <div class="col">
           <div class="card shadow-sm pt-1">
               <h4 class="card-title text-center">Stitch Funko Pop</h4>
-            <img src="../../images/st.jpg" alt="Stitch Funko Pop">
+            <img class="img-fluid" src="../../images/st.jpg" alt="Stitch Funko Pop">
             <div class="card-body">
               <p class="card-text text-center">Experiment 626<br>
                   <small class="fw-bold">$13.99</small></p>
@@ -57,7 +57,7 @@ if (empty($_SESSION['cart'])) {
         <div class="col">
           <div class="card shadow-sm pt-1">
               <h4 class="card-title text-center">Bill Nye Funko Pop</h4>
-            <img src="../../images/bill.jpg" alt="Bill Nye Funko Pop">
+            <img class="img-fluid" src="../../images/bill.jpg" alt="Bill Nye Funko Pop">
             <div class="card-body">
               <p class="card-text text-center">Science Rules<br>
                   <small class="fw-bold">$9.99</small></p>
@@ -73,7 +73,7 @@ if (empty($_SESSION['cart'])) {
         <div class="col">
           <div class="card shadow-sm pt-1">
               <h4 class="card-title text-center">Forky Funko Pop</h4>
-            <img src="../../images/fork.jpg" alt="Forky Funko Pop">
+            <img class="img-fluid" src="../../images/fork.jpg" alt="Forky Funko Pop">
             <div class="card-body">
               <p class="card-text text-center">"I am not a toy!"<br>
                   <small class="fw-bold">$15.99</small></p>
@@ -90,7 +90,7 @@ if (empty($_SESSION['cart'])) {
         <div class="col">
           <div class="card shadow-sm pt-1">
               <h4 class="card-title text-center">Winifred Sanderson Funko Pop</h4>
-            <img src="../../images/jp2.jpg" alt="Winifred Funko Pop">
+            <img class="img-fluid" src="../../images/jp2.jpg" alt="Winifred Funko Pop">
             <div class="card-body">
               <p class="card-text text-center">"I'll put a spell on you!"<br>
                   <small class="fw-bold">$16.99</small></p>
@@ -106,7 +106,7 @@ if (empty($_SESSION['cart'])) {
         <div class="col">
           <div class="card shadow-sm pt-1">
               <h4 class="card-title text-center">Mary Sanderson Funko Pop</h4>
-            <img src="../../images/hp1.jpg" alt="Mary Funko Pop">
+            <img class="img-fluid" src="../../images/hp1.jpg" alt="Mary Funko Pop">
             <div class="card-body">
               <p class="card-text text-center">"It reeks of children!"<br>
                   <small class="fw-bold">$13.99</small></p>
@@ -122,7 +122,7 @@ if (empty($_SESSION['cart'])) {
         <div class="col">
           <div class="card shadow-sm pt-1">
               <h4 class="card-title text-center">Sarah Sanderson Funko Pop</h4>
-            <img src="../../images/hp3.jpg" alt="Sarah Funko Pop">
+            <img class="img-fluid" src="../../images/hp3.jpg" alt="Sarah Funko Pop">
             <div class="card-body">
               <p class="card-text text-center">"Dead man’s toe! Dead man’s toe!"<br>
                   <small class="fw-bold">$13.99</small></p>
@@ -139,7 +139,7 @@ if (empty($_SESSION['cart'])) {
         <div class="col">
           <div class="card shadow-sm pt-1">
               <h4 class="card-title text-center">Frozone Funko Pop</h4>
-            <img src="../../images/fro.jpg" alt="Frozone Funko Pop">
+            <img class="img-fluid" src="../../images/fro.jpg" alt="Frozone Funko Pop">
             <div class="card-body">
               <p class="card-text text-center">"Woman Where Is My Super Suit!"<br>
                   <small class="fw-bold">$7.99</small></p>
@@ -155,7 +155,7 @@ if (empty($_SESSION['cart'])) {
         <div class="col">
           <div class="card shadow-sm pt-1">
             <h4 class="card-title text-center">Jim Henson Funko Pop</h4>
-            <img src="../../images/jh.jpg" alt="Jim Henson Funko Pop">
+            <img class="img-fluid" src="../../images/jh.jpg" alt="Jim Henson Funko Pop">
             <div class="card-body">
               <p class="card-text text-center">"It's not easy being green"<br>
                   <small class="fw-bold">$17.99</small></p>
@@ -171,7 +171,7 @@ if (empty($_SESSION['cart'])) {
         <div class="col">
           <div class="card shadow-sm pt-1">
               <h4 class="card-title text-center">Little Green Alien Funko Pop</h4>
-            <img src="../../images/al.jpg" alt="Alien Funko Pop">
+            <img class="img-fluid" src="../../images/al.jpg" alt="Alien Funko Pop">
             <div class="card-body">
               <p class="card-text text-center">"The Claw Is Our Master!"<br>
                   <small class="fw-bold">$8.99</small></p>
