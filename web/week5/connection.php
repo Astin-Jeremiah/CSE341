240917ac0e372e00c285218e6e5f084672f87222
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-02 22:03:05 -0500

    add dbConnect

---
+++ web/week5/connection.php
@@ -0,0 +1,41 @@
+<?php
+require('../modules/dbConnect.php');
+$db = get_db();
+
+$query = 'SELECT public.content.content_name, public.content.description, public.service.service_name FROM public.content';
+$stmt = $db->prepare($query);
+$stmt->execute();
+$content = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Connection Test</title>
+
+      <link href="../../bootstrap.min.css" rel="stylesheet">
+    <link href="form-validation.css" rel="stylesheet">
+  </head>
+  <body class="bg-light">
+    
+<div class="container">
+  <h1>Content</h1>
+    
+    <ul>
+        <?php
+            foreach ($content as $con)
+            {
+                $name = $content['name'];
+                $des = $content['descirption'];
+                $service = $content['service_name'];
+                echo "<li><p>$name&emsp;$des&emsp;$service</p></li>";
+            }
+        ?>
+        <li></li>
+    </ul>
+
+</div>
+
+  </body>
+</html>
