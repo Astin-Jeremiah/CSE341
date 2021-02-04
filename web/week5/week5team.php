8d08084f6d4ef55f2af74a5094cb0723b3d62109
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-03 20:23:16 -0500

    upload page

--- web/week5/week5team.php
+++ web/week5/week5team.php
@@ -3,10 +3,10 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
 $db = get_db();
 
-$query = 'SELECT public.content.content_name, public.content.description, public.service.service_name FROM public.content INNER JOIN public.service ON public.service.id = public.content.service_id';
+$query = 'SELECT * FROM public.scripture'
 $stmt = $db->prepare($query);
 $stmt->execute();
-$content = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$scripture = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!doctype html>
 <html lang="en">
@@ -21,19 +21,18 @@ $content = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <body class="bg-light">
     
 <div class="container">
-  <h1>Content</h1>
-    
-    <ul>
+  <h1>Scripture References</h1>
+
         <?php
-            foreach ($content as $con)
+            foreach ($scripture as $script)
             {
-                $name = $con['content_name'];
-                $des = $con['description'];
-                $service = $con['service_name'];
-                echo "<li><p>$name&nbsp;&#8212;&nbsp;$des&nbsp;&#8212;&nbsp;$service</p></li>";
+                $book = $script['book'];
+                $chapter = $script['chapter'];
+                $verse = $script['verse'];
+                $content = $script['content'];
+                echo "<p><b>$book&nbsp;$chapter:$verse - \"$content\"</b></p>";
             }
         ?>
-    </ul>
 
 </div>
 
