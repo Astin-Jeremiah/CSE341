db958bb210842c41c1934dbfb609cc4567f525a3
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-03 20:47:11 -0500

    update

--- web/week5/teamresults.php
+++ web/week5/teamresults.php
@@ -2,28 +2,26 @@
 
 require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
 $db = get_db();
-
-$query = 'SELECT * FROM public.scripture';
+$book = htmlspecialchars($_POST["book"]);
+$query = 'SELECT * FROM public.scripture WHERE public.scripture.book = $book';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $scripture = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Week 5 Team Activity</title>
-
-      <link href="../../bootstrap.min.css" rel="stylesheet">
-    <link href="form-validation.css" rel="stylesheet">
+    <title>Results</title>
   </head>
   <body class="bg-light">
     
 <div class="container">
-  <h1>Scripture References</h1>
-
-        <?php
+  <h1>Scripture Results</h1>
+    
+    <?php
             foreach ($scripture as $script)
             {
                 $book = $script['book'];
--- web/week5/teamstretch.php
+++ web/week5/teamstretch.php
@@ -7,6 +7,8 @@ $query = 'SELECT * FROM public.scripture';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $scripture = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+<
 ?>
 <!doctype html>
 <html lang="en">
@@ -22,17 +24,11 @@ $scripture = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
 <div class="container">
   <h1>Scripture References</h1>
-
-        <?php
-            foreach ($scripture as $script)
-            {
-                $book = $script['book'];
-                $chapter = $script['chapter'];
-                $verse = $script['verse'];
-                $content = $script['content'];
-                echo "<p><b>$book&nbsp;$chapter:$verse</b> - \"$content\"</p>";
-            }
-        ?>
+    
+    <form action="teamresults.php" method="post">
+        Book:<input type="text" name="book">
+        <input type=submit value="Search">
+    </form>
 
 </div>
 
