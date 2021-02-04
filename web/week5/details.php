65e4817e090bbaac9194df8caa5465cfe31c4de4
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-03 22:10:40 -0500

    fix

--- web/week5/details.php
+++ web/week5/details.php
@@ -2,6 +2,19 @@
 
 require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
 
+if (!issset($_GET['course_id']))
+{
+    die("Error, ID not specified");
+}
+
+$sid = htmlspecialchars($_GET['scripture_id']);
+
+$db = get_db();
+$query = 'SELECT * FROM scripture WHERE id = \''.$sid.'\'';
+$stmt = $db->prepare($query);
+$stmt->bindValue(':id', $sid, PDO::PARAM_INT);
+$stmt->execute();
+$scripture = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 
@@ -17,6 +30,17 @@ require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
     
 <div class="container">
   <h1>Scripture Details</h1>
+    
+    <?php
+            foreach ($scripture as $script)
+            {
+                $book = $script['book'];
+                $chapter = $script['chapter'];
+                $verse = $script['verse'];
+                $content = $script['content'];
+                echo "<p><b>$book&nbsp;$chapter:$verse</b> - \"$content\"</p>";
+            }
+        ?>
     
      
 </div>
