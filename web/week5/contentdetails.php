94a8a822121d05395a5e6ff38bd770830ac616bb
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-04 21:42:02 -0500

    adjust layout

--- web/week5/contentdetails.php
+++ web/week5/contentdetails.php
@@ -60,14 +60,8 @@ $details = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 </div>
                 </div>
                 </div>
-                <div class='row g-0'>
-                <div class='col-md-8'>
-                <div class='card-body'>
                 <h3 class='card-title'>Reviews</h3>
-                <p class='card-text-center'><b>$rev</b>&#8212; $fn $ln</p>
-                </div>
-                </div>
-                </div>
+                <p class='card-text-center'><b>$rev</b> &#8212; $fn $ln</p>
                 </div>";
                 
             }
