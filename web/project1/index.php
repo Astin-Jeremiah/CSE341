caa915125673db2ff8680c1e2e71677e976efa92
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-05 22:54:25 -0500

    try

--- web/project1/index.php
+++ web/project1/index.php
@@ -3,63 +3,8 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
 $db = get_db();
 
-$query = 'SELECT * FROM content ORDER BY content_name ASC';
-$stmt = $db->prepare($query);
-$stmt->execute();
-$content = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-$query2 = 'SELECT * FROM service';
-$stmt2 = $db->prepare($query2);
-$stmt2->execute();
-$serv = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-?>
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Stream Central</title>
-
-    <link href="../../bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link href="style.css" rel="stylesheet">
-</head>
-
-<body class="bg-secondary">
-    <header>
-        <div class="container text-end">
-            <a href="#">
-                <i class="bi bi-person-circle" id="login"></i>
-            </a>
-        </div>
-    </header>
-
-    <main>
-        <div class="py-3 text-center">
-            <img class="img-fluid" src="../../images/logo.png" alt="Stream Central Logo">
-            <h3>Find Something New To Watch</h3>
-            <br>
-            <h1>Popular Shows</h1>
-        </div>
-        <div class="album">
-            <div class="container">
-                <form action="" method="post">
-                    <label for="cars">Filter By Streaming Service:</label>
-                    <select name="service" id="service">
-                        <option value=''>All</option>
-                        <?php
-            foreach ($serv as $service)
-            {
-                $id2 = $service['id'];
-                $servicename = $service['service_name'];
-                echo "<option value='$id2'>$servicename</option>";
-            }
-        ?>
-                    </select>
-                    <input type="submit" type="submit">
-                    <?php
 $serid = htmlspecialchars($_GET['value']);
+
 if ($serid = "1") {
   $query =  'SELECT * FROM content WHERE service_id = 1 ORDER BY content_name ASC';
 } elseif ($serid = "2") {
@@ -85,12 +30,65 @@ if ($serid = "1") {
 } else {
   $query =  'SELECT * FROM content ORDER BY content_name ASC';
 }
-       ?>
 
-                </form>
-                <br>
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-md-4 g-3">
-                    <?php
+
+$stmt = $db->prepare($query);
+$stmt->execute();
+$content = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$query2 = 'SELECT * FROM service';
+$stmt2 = $db->prepare($query2);
+$stmt2->execute();
+$serv = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Stream Central</title>
+
+      <link href="../../bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+      <link href="style.css" rel="stylesheet">
+  </head>
+  <body class="bg-secondary">
+ <header>
+    <div class="container text-end">
+      <a href="#">
+        <i class="bi bi-person-circle" id="login"></i>
+      </a>
+  </div>
+</header>
+
+      <main>
+<div class="py-3 text-center">
+      <img class="img-fluid" src="../../images/logo.png" alt="Stream Central Logo">
+        <h3>Find Something New To Watch</h3>
+        <br>
+      <h1>Popular Shows</h1>
+    </div>      
+<div class="album">      
+<div class="container">
+   <form action="" method="post">
+    <label for="cars">Filter By Streaming Service:</label>
+  <select name="service" id="service">
+      <option value=''>All</option>
+      <?php
+            foreach ($serv as $service)
+            {
+                $id2 = $service['id'];
+                $servicename = $service['service_name'];
+                echo "<option value='$id2'>$servicename</option>";
+            }
+        ?>
+  </select>
+  <input type="submit" type="submit">
+    
+</form>
+<br>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-md-4 g-3">
+        <?php
             foreach ($content as $con)
             {
                 $id = $con['id'];
@@ -103,10 +101,9 @@ if ($serid = "1") {
         </div>";
             }
         ?>
-                </div>
-            </div>
-        </div>
-    </main>
-</body>
-
+</div>
+      </div>
+      </div>
+      </main>
+  </body>
 </html>
