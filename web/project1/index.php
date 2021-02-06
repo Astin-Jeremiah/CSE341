2a428b31fedad6bbc5dd066d7f3f6234c8d5070b
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-05 23:11:35 -0500

    intval

--- web/project1/index.php
+++ web/project1/index.php
@@ -3,9 +3,7 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
 $db = get_db();
 
-$serid = htmlspecialchars($_GET['value']);
-
-echo $serid;
+$serid = intval($_GET['value']);
 
 if ($serid == 1) {
   $query =  'SELECT * FROM content WHERE service_id = 1 ORDER BY content_name ASC';
