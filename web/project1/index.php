cafd7b1197a8b5bb246b8958f8f948d48b0ad2ba
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-06 00:11:24 -0500

    ??

--- web/project1/index.php
+++ web/project1/index.php
@@ -84,7 +84,7 @@ $serv = $stmt2->fetchAll(PDO::FETCH_ASSOC);
             {
                 $id2 = $service['id'];
                 $servicename = $service['service_name'];
-                echo "<option value='$id2' (isset($_POST['service']) && $_POST['service'] == '$id2') ? 'selected' : ''>$servicename</option>";
+                echo "<option value='$id2' if($_GET['service'] == '$id2'){ php?>selected<?php }>$servicename</option>";
             }
         ?>
   </select>
