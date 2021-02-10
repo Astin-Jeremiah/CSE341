e1962cf19bcf8a5c2eccdee7e55035ad64c5c5ea
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-09 23:08:37 -0500

    add register page

--- web/project1/register.php
+++ web/project1/register.php
@@ -9,7 +9,7 @@ $db = get_db();
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Account Login</title>
+    <title>Account Registration</title>
 
       <link href="../../bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
@@ -34,22 +34,29 @@ $db = get_db();
                 <h4 class="mb-3">Login Information:</h4>
                 <form class="needs-validation" action="" method="" novalidate>
                     <div class="row g-3">
+                        <div class="col-md-6">
+                            <label for="username" class="form-label">User Name</label>
+                            <input type="text" class="form-control" id="username" name="username" required>
+                            <div class="invalid-feedback">
+                                User Name is required.
+                            </div>
+                        </div>
                         <div class="col-12">
                             <label for="email" class="form-label">Email</label>
                             <input type="email" class="form-control" id="email" name="email" required>
                             <div class="invalid-feedback">
-                                Please enter a valid email address for shipping updates.
+                                Please enter a valid email address.
                             </div>
                         </div>
                       <div class="col-12">
                             <label for="password" class="form-label">Password</label>
                             <input type="password" class="form-control" id="password" name="password" required>
+                          <div class="invalid-feedback">
+                                Password is required.
+                            </div>
                         </div>  
                     </div><br>
-                        <input type="submit" class="btn btn-dark" value="Login">
-                <br><br>
-                <h4 class="mb-3">Not Registered Yet?</h4>
-                <a class="btn btn-dark" href="register.php" role="button">Register For New Account</a>
+                        <input type="submit" class="btn btn-dark" value="Create Account">
             </form>    
           </div>   
       </main>
