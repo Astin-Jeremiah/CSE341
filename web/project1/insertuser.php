d2db3dc9f33644c5e95b54f5fd681da5c2daa85e
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-09 23:29:26 -0500

    fix db connect

--- web/project1/insertuser.php
+++ web/project1/insertuser.php
@@ -5,10 +5,10 @@ $uname = htmlspecialchars($_POST['username']);
 $email = htmlspecialchars($_POST['email']);
 $pword = htmlspecialchars($_POST['password']);
 
-require('dbConnect.php');
+require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
 $db = get_db();
 
-$stmt = $db->prepare('INSERT INTO account(user_name, email, password) VALUES (:user_name, :email, :password);');
+$stmt = $db->prepare('INSERT INTO account (user_name, email, password) VALUES (:user_name, :email, :password);');
 $stmt->bindValue(':user_name', $uname, PDO::PARAM_STR);
 $stmt->bindValue(':email', $email, PDO::PARAM_STR);
 $stmt->bindValue(':password', $pword, PDO::PARAM_STR);
