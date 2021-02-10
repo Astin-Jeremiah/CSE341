23e13d19c42092a019f75268a8596cb926dfd013
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-09 23:22:37 -0500

    fix sql

--- web/project1/insertuser.php
+++ web/project1/insertuser.php
@@ -8,8 +8,8 @@ $pword = htmlspecialchars($_POST['password']);
 require('dbConnect.php');
 $db = get_db();
 
-$stmt = $db->prepare('INSERT INTO account(user_name, email, password) VALUES (:username, :email, :password);');
-$stmt->bindValue(':username', $uname, PDO::PARAM_STR);
+$stmt = $db->prepare('INSERT INTO account(user_name, email, password) VALUES (:user_name, :email, :password);');
+$stmt->bindValue(':user_name', $uname, PDO::PARAM_STR);
 $stmt->bindValue(':email', $email, PDO::PARAM_STR);
 $stmt->bindValue(':password', $pword, PDO::PARAM_STR);
 $stmt->execute();
