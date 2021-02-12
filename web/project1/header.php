5f2a1b5044781d3bd728410d23c79e5e5af112d2
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 14:42:22 -0500

    add css

--- web/project1/header.php
+++ web/project1/header.php
@@ -2,6 +2,6 @@
         <a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>
           <?php
     if (isset ($_SESSION['username'])){
-        echo "|<span><a href='logoff.php' title='logout'>Logout</a></span>";
+        echo "|<span id='header'><a href='logoff.php' title='logout'>Logout</a></span>";
     }?>
 </div>
