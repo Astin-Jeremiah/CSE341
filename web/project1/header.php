6125fea7d9030bb2dd76d5d44236e28f6055f413
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 15:06:39 -0500

    fix styling

--- web/project1/header.php
+++ web/project1/header.php
@@ -2,6 +2,6 @@
         <a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>
           <?php
     if (isset ($_SESSION['username'])){
-        echo "<span id='header'>|</span><a href='logoff.php' title='logout' id='out'><div id='logout'><h4>Logout</h4></div></a>";
+        echo "<span id='header'>|</span><a href='logoff.php' title='logout' id='out'><span class='fs-4'>Logout</span></a>";
     }?>
 </div>
