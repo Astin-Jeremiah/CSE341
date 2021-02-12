f1258c3d7438f459bfb27d33c18a02046b0de9e4
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 15:16:38 -0500

    improve header

--- web/project1/header.php
+++ web/project1/header.php
@@ -1,7 +1,9 @@
 <div class="container text-end">
-        <a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>
           <?php
     if (isset ($_SESSION['username'])){
-        echo "<span id='header'>|</span><a href='logoff.php' title='logout' id='out'><span class='fs-4 fw-bold'>Logout</span></a>";
-    }?>
+        echo "<a href='accountinfo.php'><i class='bi bi-person-circle' id='login'></i></a><span id='header'>|</span><a href='logoff.php' title='logout' id='out'><span class='fs-4 fw-bold'>Logout</span></a>";
+    } else {
+        echo "<a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>";
+    }
+    ?>
 </div>
