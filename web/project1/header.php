67556c0e88c2f94384fd4dfe23e7b33ee6476f59
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 15:51:20 -0500

    fix css

--- web/project1/header.php
+++ web/project1/header.php
@@ -1,5 +1,5 @@
 <div class="container text-end">
-    <a href='index.php'><i class="bi bi-house-door-fill"></i></a>
+    <a href='index.php'><i class="bi bi-house-door-fill" id='home'></i></a>
           <?php
     if (isset ($_SESSION['username'])){
         echo "<span id='header'>|</span><a href='accountinfo.php'><i class='bi bi-person-circle' id='login'></i></a><span id='header'>|</span><a href='logoff.php' title='logout' id='out'><span class='fs-4 fw-bold'>Logout</span></a>";
