6b3ecfa3a3895186c86439a5519d830ff64c2d04
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-11 23:38:57 -0500

    try new layout

--- web/project1/register.php
+++ web/project1/register.php
@@ -1,7 +1,41 @@
 <?php
 
 require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
+
+$uname = htmlspecialchars($_POST['username']);
+$email = htmlspecialchars($_POST['email']);
+$pword = password_hash($password, PASSWORD_DEFAULT);
+$existinguname = checkExistinguname($uname);
+
+$regOutcome = regClient($uname, $email, $pword);
+
+function checkExistinguname($uname) {
+ $db = get_db();
+ $stmt2 = $db->prepare('SELECT user_name FROM account WHERE user_name = :uname');
+ $stmt2->bindValue(':uname', $uname, PDO::PARAM_STR);
+ $stmt2->execute();
+ $matchname = $stmt2->fetch(PDO::FETCH_NUM);
+ if($matchname >= 1){
+ $message = '<p class="message">Username already exists</p>';
+ exit;
+}
+}
+
+
+function regClient($uname, $email, $pword) {
 $db = get_db();
+$stmt = $db->prepare('INSERT INTO account (user_name, email, password) VALUES (:user_name, :email, :password);');
+$stmt->bindValue(':user_name', $uname, PDO::PARAM_STR);
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+$stmt->bindValue(':password', $pword);
+$stmt->execute();
+    
+$new_page = "login.php";    
+header("Location: $new_page");
+    
+die();
+
+}
 
 ?>
 <!doctype html>
@@ -35,7 +69,7 @@ $db = get_db();
                     }
                 ?>
                 </div>
-                <form action="insertuser.php" method="post">
+                <form action="register.php" method="post">
                     <div class="row g-3">
                         <div class="col-md-6">
                             <label for="username" class="form-label">User Name</label>
