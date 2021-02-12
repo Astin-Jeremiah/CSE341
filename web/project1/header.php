057323277f2ed8abc3177912e14f3f44b8d0ef34
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 14:52:21 -0500

    fix typo

--- web/project1/header.php
+++ web/project1/header.php
@@ -2,6 +2,6 @@
         <a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>
           <?php
     if (isset ($_SESSION['username'])){
-        echo "<sppanid='header'>|</span><a href='logoff.php' title='logout' id='out'><h4>Logout</h4></a>";
+        echo "<span id='header'>|</span><a href='logoff.php' title='logout' id='out'><h4>Logout</h4></a>";
     }?>
 </div>
