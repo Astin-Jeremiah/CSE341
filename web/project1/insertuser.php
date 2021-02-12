b5ae8b06bd6a776452cded7d92dec7472587f02d
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-11 22:13:01 -0500

    fix sql

--- web/project1/insertuser.php
+++ web/project1/insertuser.php
@@ -1,11 +1,48 @@
   
 <?php
 
+require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
+
 $uname = htmlspecialchars($_POST['username']);
 $email = htmlspecialchars($_POST['email']);
 $pword = password_hash($password, PASSWORD_DEFAULT);
-$existingemail = checkExistingEmail($email);
 $existinguname = checkExistinguname($uname);
+$existingemail = checkExistingEmail($email);
+
+function checkExistinguname($uname) {
+ $db = get_db();
+ $sql = 'SELECT user_name FROM account WHERE user_name = :user_name';
+ $stmt = $db->prepare($sql);
+ $stmt->bindValue(':user_name', $uname, PDO::PARAM_STR);
+ $stmt->execute();
+ $matchname = $stmt->fetch(PDO::FETCH_NUM);
+ $stmt->closeCursor();
+ if(empty($matchname)){
+ return 0;
+
+} else {
+ return 1;
+
+}
+}
+
+function checkExistingEmail($email) {
+ $db = get_db();
+ $sql = 'SELECT email FROM account WHERE email = :email';
+ $stmt = $db->prepare($sql);
+ $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+ $stmt->execute();
+ $matchEmail = $stmt->fetch(PDO::FETCH_NUM);
+ $stmt->closeCursor();
+ if(empty($matchEmail)){
+ return 0;
+
+} else {
+ return 1;
+
+}
+}
+
 
 if($existingemail){
         $message = '<p class="message">That email address already exists.</p>';
@@ -17,9 +54,9 @@ if($existinguname){
     exit;
     }
 
-require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
-$db = get_db();
 
+
+$db = get_db();
 $stmt = $db->prepare('INSERT INTO account (user_name, email, password) VALUES (:user_name, :email, :password);');
 $stmt->bindValue(':user_name', $uname, PDO::PARAM_STR);
 $stmt->bindValue(':email', $email, PDO::PARAM_STR);
@@ -33,38 +70,8 @@ header("Location: $new_page");
 die();
 
 // Check for an existing email address
-function checkExistingEmail($email) {
- $db = get_db();
- $sql = 'SELECT email FROM account WHERE email = :email';
- $stmt = $db->prepare($sql);
- $stmt->bindValue(':email', $clientEmail, PDO::PARAM_STR);
- $stmt->execute();
- $matchEmail = $stmt->fetch(PDO::FETCH_NUM);
- $stmt->closeCursor();
- if(empty($matchEmail)){
- return 0;
 
-} else {
- return 1;
 
-}
-}
 
-function checkExistinguname($uname) {
- $db = get_db();
- $sql = 'SELECT user_name FROM account WHERE user_name = :user_name';
- $stmt = $db->prepare($sql);
- $stmt->bindValue(':email', $clientEmail, PDO::PARAM_STR);
- $stmt->execute();
- $matchEmail = $stmt->fetch(PDO::FETCH_NUM);
- $stmt->closeCursor();
- if(empty($matchEmail)){
- return 0;
-
-} else {
- return 1;
-
-}
-}
 
 ?>
