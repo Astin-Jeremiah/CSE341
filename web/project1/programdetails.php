5621d63c904fd894ed389ce1e29c0e07290f0ae2
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 16:31:50 -0500

    fix

--- web/project1/programdetails.php
+++ web/project1/programdetails.php
@@ -21,7 +21,7 @@ $stmt2->bindValue(':id', $sid, PDO::PARAM_INT);
 $stmt2->execute();
 $review = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 
-
+echo var_dump($_SESSION);
 
 ?>
 <!doctype html>
@@ -89,7 +89,7 @@ $review = $stmt2->fetchAll(PDO::FETCH_ASSOC);
                 echo "<p class='card-text'><b>$note</b> &#8212; $uname</p>";
                 }
                 ?> 
-                <a href='#' class='btn btn-dark me-2' role='button'>Write A Review</a>
+                <a href='login.php' class='btn btn-dark me-2' role='button'>Login To Write A Review</a>
                 </div>
                 </div>
                 </div>
