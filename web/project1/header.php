012c10cef2c194fb1d584a7ef22c5ca902d5da4f
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 15:47:04 -0500

    add home icon

--- web/project1/header.php
+++ web/project1/header.php
@@ -1,9 +1,10 @@
 <div class="container text-end">
+    <a href='index.php'><i class="bi bi-house-door-fill"></i></a>
           <?php
     if (isset ($_SESSION['username'])){
-        echo "<a href='accountinfo.php'><i class='bi bi-person-circle' id='login'></i></a><span id='header'>|</span><a href='logoff.php' title='logout' id='out'><span class='fs-4 fw-bold'>Logout</span></a>";
+        echo "<span id='header'>|</span><a href='accountinfo.php'><i class='bi bi-person-circle' id='login'></i></a><span id='header'>|</span><a href='logoff.php' title='logout' id='out'><span class='fs-4 fw-bold'>Logout</span></a>";
     } else {
-        echo "<a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>";
+        echo " <a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>";
     }
     ?>
 </div>
