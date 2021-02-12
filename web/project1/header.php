8614c1b3f9d623a152a49a9de45b29c134a877d3
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 15:58:20 -0500

    fix typo

--- web/project1/header.php
+++ web/project1/header.php
@@ -2,7 +2,7 @@
     <a href='index.php'><i class="bi bi-house-door-fill" id='home'></i></a>
           <?php
     if (isset ($_SESSION['username'])){
-        echo "<a href='accountinfo.php'><i class='bi bi-person-circle' id='login'></i></a> <a href='logout.php'><i class='bi bi-person-circle' id='login' id='out'></i></a>;
+        echo "<a href='accountinfo.php'><i class='bi bi-person-circle' id='login'></i></a> <a href='logout.php'><i class='bi bi-person-circle' id='login' id='out'></i></a>";
     } else {
         echo " <a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>";
     }
