24d77baf8dfe5193ff01632a97439a018a3146f8
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 16:36:43 -0500

    try

--- web/project1/programdetails.php
+++ web/project1/programdetails.php
@@ -21,7 +21,7 @@ $stmt2->bindValue(':id', $sid, PDO::PARAM_INT);
 $stmt2->execute();
 $review = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 
-echo var_dump($_SESSION['username']);
+echo $username;
 
 ?>
 <!doctype html>
