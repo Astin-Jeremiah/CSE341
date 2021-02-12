acdaa7ff909773016d72ebd175742747231bfb3e
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 15:09:57 -0500

    fix class

--- web/project1/header.php
+++ web/project1/header.php
@@ -2,6 +2,6 @@
         <a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>
           <?php
     if (isset ($_SESSION['username'])){
-        echo "<span id='header'>|</span><a href='logoff.php' title='logout' id='out'><span class='fs-4'>Logout</span></a>";
+        echo "<span id='header'>|</span><a href='logoff.php' title='logout' id='out'><span class='fs-4 fw-bold'>Logout</span></a>";
     }?>
 </div>
