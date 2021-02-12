84c27877eb06f20f7827cc69e0a69bc8ee51edc5
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 17:20:24 -0500

    change session name

--- web/project1/header.php
+++ web/project1/header.php
@@ -1,7 +1,7 @@
 <div class="container text-end">
     <a href='index.php'><i class="bi bi-house-door-fill" id='home'></i></a>
           <?php
-    if (isset ($_SESSION['username'])){
+    if (isset ($_SESSION['userid'])){
         echo "<a href='accountinfo.php'><i class='bi bi-person-circle' id='login'></i></a> <a href='logoff.php'><i class='bi bi-door-closed-fill' id='out'></i></a>";
     } else {
         echo " <a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>";
