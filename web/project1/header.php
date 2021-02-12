764ec288fa17fd3e9c0eb85c50bb14bb4b80aecf
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-11 20:13:57 -0500

    header style

--- web/project1/header.php
+++ web/project1/header.php
@@ -1,5 +1,10 @@
 <div class="container text-end">
-      <a href="login.php">
         <i class="bi bi-person-circle" id="login"></i>
-      </a>
+          <?php
+    if (isset ($_SESSION['loggedin'])){
+        echo "<a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>|</p><span><a href='#' title='logout'>Logout</a></span>";
+    } else {
+        echo "<a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>";
+    }?>
+      
 </div>
