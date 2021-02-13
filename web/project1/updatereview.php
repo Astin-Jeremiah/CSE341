87ee2e41e68ec6c9a4cb4102a003b076e2999ec3
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-13 16:25:33 -0500

    fix typo

--- web/project1/updatereview.php
+++ web/project1/updatereview.php
@@ -5,14 +5,14 @@ $userid2 = htmlspecialchars($_POST['aid']);
 $programid2 = htmlspecialchars($_POST['cid']);
 
 require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
-    $db = get_db();
-    $stmt = $db->prepare('UPDATE reviews SET note = :review WHERE account_id = :userid AND content_id = :programid');
-    $stmt->bindValue(':review', $nrev);
-    $stmt->bindValue(':userid', $userid2);
-    $stmt->bindValue(':programid', $programid2);
-    $stmt->execute();
+$db = get_db();
+$stmt = $db->prepare('UPDATE reviews SET note = :review WHERE account_id = :userid AND content_id = :programid');
+$stmt->bindValue(':review', $nrev);
+$stmt->bindValue(':userid', $userid2);
+$stmt->bindValue(':programid', $programid2);
+$stmt->execute();
 
-    $new_page = "accountinfo.php";    
-    header("Location: $new_page");
-    die();   */
+$new_page = "accountinfo.php";    
+header("Location: $new_page");
+die();
 ?>
