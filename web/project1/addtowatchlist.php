e388331c232548429a48d1021b289470e292d8b3
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-13 00:54:57 -0500

    add sql

--- web/project1/addtowatchlist.php
+++ web/project1/addtowatchlist.php
@@ -10,30 +10,11 @@ $programid = htmlspecialchars($_POST['proid']);
 $uid = $_SESSION['userid'];
 $date = date("m/d/Y");
 
-echo $programid;
-echo $uid;
-echo $date;
-
-/*echo ($_POST["userid"]);
-echo ($_POST["programid"]);
-echo ($_POST["review"]);
-
-require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
-
-$userid = htmlspecialchars($_POST['userid']);
-$programid = htmlspecialchars($_POST['programid']);
-$review = htmlspecialchars($_POST['review']);
-
 $db = get_db();
-$stmt = $db->prepare('INSERT INTO reviews (account_id, content_id, note) VALUES (:acid, :conid, :rev);');
-$stmt->bindValue(':acid', $userid);
-$stmt->bindValue(':conid', $programid);
-$stmt->bindValue(':rev', $review);
+$stmt = $db->prepare('INSERT INTO userq (account_id, content_id, date) VALUES (:acid, :conid, :date);');
+$stmt->bindValue(':acid', $programid);
+$stmt->bindValue(':conid', $uid);
+$stmt->bindValue(':date', $date);
 $stmt->execute();
-    
-$new_page = "programdetails.php?id=$programid";    
-header("Location: $new_page");
-    
-die();*/
 }
 ?>
