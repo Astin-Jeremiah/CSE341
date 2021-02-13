3727dcf77d97778fd09a92f5354e8e5250892881
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 20:12:34 -0500

    final

--- web/project1/header.php
+++ web/project1/header.php
@@ -2,9 +2,9 @@
     <a href='index.php'><i class="bi bi-house-door-fill" id='home' title='Home'></i></a>
           <?php
     if (isset ($_SESSION['userid'])){
-        echo "<a href='accountinfo.php'><i class='bi bi-person-circle' id='login' title='Log In/Account'></i></a> <a href='logoff.php'><i class='bi bi-door-closed-fill' id='out'></i></a>";
+        echo "<a href='accountinfo.php'><i class='bi bi-person-circle' id='login' title='Account Information'></i></a> <a href='logoff.php'><i class='bi bi-door-closed-fill' id='out' title='Logout'></i></a>";
     } else {
-        echo " <a href='login.php'><i class='bi bi-person-circle' id='login' title='Logout'></i></a>";
+        echo " <a href='login.php'><i class='bi bi-person-circle' id='login' title='Login'></i></a>";
     }
     ?>
 </div>
