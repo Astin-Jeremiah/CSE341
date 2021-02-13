9f8d1a48157400aa5517d6f6c711cf35472c4685
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-13 16:15:23 -0500

    troubleshoot

--- web/project1/updatereview.php
+++ web/project1/updatereview.php
@@ -7,20 +7,4 @@ $programid2 = htmlspecialchars($_POST['cid']);
 echo $nrev;
 echo $userid2;
 echo $programid2;
-
-/*if (isset($_POST['newreview']) && isset($_POST['aid']) && isset ($_POST['cid']))
-{
-    
-    require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
-    $db = get_db();
-    $stmt = $db->prepare('UPDATE reviews SET note = :review WHERE account_id = :userid AND content_id = :programid;');
-    $stmt->bindValue(':review', $nrev);
-    $stmt->bindValue(':userid', $userid2);
-    $stmt->bindValue(':programid', $programid2);
-    $stmt->execute();
-
-    $new_page = "accountinfo.php";    
-    header("Location: $new_page");
-    die();   */
-}*/
 ?>
