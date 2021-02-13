fc36558dc7884139f873547454c3491430931056
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-13 17:04:39 -0500

    finalize

--- web/project1/editreview.php
+++ web/project1/editreview.php
@@ -30,19 +30,19 @@ $review = htmlspecialchars($_POST['review']);
                 <h4 class="mb-3">Edit Review:</h4>
                     <div class="row g-3">
                         <div class="col-12">
-                            <form  id="specialform" action="updatereview.php" method="POST" style="display:inline-block;">
+                            <form action="updatereview.php" method="POST">
                             <input type='hidden' id='aid' name='aid' value='<?php echo $userid; ?>'>
                             <input type='hidden' id='cid' name='cid' value='<?php echo $programid; ?>'>
                             <textarea class='form-control' placeholder='Review' id='newreview' name='newreview'><?php echo $review; ?></textarea>
                             <br>
-                            <div>
+                            <div class="col-lg-6">
                             <input type='submit' class='btn btn-dark' value='Submit Review'>
                             </div>
                             </form><br>
-                            <form  id="specialform" action="deletereview.php" method="POST" style="display:inline-block;">
+                            <form  action="deletereview.php" method="POST">
                             <input type='hidden' id='aid' name='aid' value='<?php echo $userid; ?>'>
                             <input type='hidden' id='cid' name='cid' value='<?php echo $programid; ?>'>
-                            <div>
+                            <div class="col-lg-6">
                             <input type='submit' class='btn btn-dark' value='Delete Review'>
                             </div>
                             </form>    
