30cd55963d9f72669a1465e6c8859d05e0653d3e
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-12 20:09:31 -0500

    add titles

--- web/project1/header.php
+++ web/project1/header.php
@@ -1,10 +1,10 @@
 <div class="container text-end">
-    <a href='index.php'><i class="bi bi-house-door-fill" id='home'></i></a>
+    <a href='index.php'><i class="bi bi-house-door-fill" id='home' title='Home'></i></a>
           <?php
     if (isset ($_SESSION['userid'])){
-        echo "<a href='accountinfo.php'><i class='bi bi-person-circle' id='login'></i></a> <a href='logoff.php'><i class='bi bi-door-closed-fill' id='out'></i></a>";
+        echo "<a href='accountinfo.php'><i class='bi bi-person-circle' id='login' title='Log In/Account'></i></a> <a href='logoff.php'><i class='bi bi-door-closed-fill' id='out'></i></a>";
     } else {
-        echo " <a href='login.php'><i class='bi bi-person-circle' id='login'></i></a>";
+        echo " <a href='login.php'><i class='bi bi-person-circle' id='login' title='Logout'></i></a>";
     }
     ?>
 </div>
