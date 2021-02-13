63d3382dfe839430f91adbd193aa020f7a2e4071
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-13 17:55:24 -0500

    test layout

--- web/project1/updateaccountinfo.php
+++ web/project1/updateaccountinfo.php
@@ -44,6 +44,7 @@ $email = htmlspecialchars($_POST['email']);
                             <form action="updateusername.php" method="post">
                             <label for="username" class="form-label">User Name</label>
                             <input type="text" class="form-control" id="username" name="username" required <?php if(isset($username)){echo "value='$username'";}  ?>>
+                            <br>
                             <input type='submit' class='btn btn-dark' value='Update User Name'>
                             </form>    
                         </div>
@@ -51,6 +52,7 @@ $email = htmlspecialchars($_POST['email']);
                             <form action="updateemail.php" method="post">
                             <label for="email" class="form-label">Email</label>
                             <input type="email" class="form-control" id="email" name="email" required <?php if(isset($email)){echo "value='$email'";}  ?>>
+                            <br>
                             <input type='submit' class='btn btn-dark' value='Update Email'>
                             </form>
                         </div>
@@ -58,6 +60,7 @@ $email = htmlspecialchars($_POST['email']);
                             <form action="updatepword.php" method="post">
                             <label for="password" class="form-label">Password - At least 8 characters containing 1 number, 1 capital letter and 1 special character.</label>
                             <input type="password" class="form-control" id="password" name="password" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
+                                <br>
                             <input type='submit' class='btn btn-dark' value='Update Password'>
                           </form>
                         </div>  
