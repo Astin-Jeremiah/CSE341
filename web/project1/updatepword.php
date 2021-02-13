c8747f07c70c2bfa11ec4fe644316517d9b76b47
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-13 18:13:36 -0500

    test

--- web/project1/updatepword.php
+++ web/project1/updatepword.php
@@ -3,9 +3,11 @@ require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
 
 $uname = htmlspecialchars($_POST['uname']);
 $pword = htmlspecialchars($_POST['password']);
+$newhash =  password_hash($pword, PASSWORD_DEFAULT);
 
 echo $uname;
 echo $pword;
+echo $newhash;
 
 
 /*$hash =  password_hash($pword, PASSWORD_DEFAULT);
