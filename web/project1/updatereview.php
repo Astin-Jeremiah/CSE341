e0d81296e06206c5bfe89029c3d455fb9d911002
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-13 16:23:56 -0500

    fix 500

--- web/project1/updatereview.php
+++ web/project1/updatereview.php
@@ -6,7 +6,7 @@ $programid2 = htmlspecialchars($_POST['cid']);
 
 require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
     $db = get_db();
-    $stmt = $db->prepare('UPDATE reviews SET note = :review WHERE account_id = :userid AND content_id = :programid;');
+    $stmt = $db->prepare('UPDATE reviews SET note = :review WHERE account_id = :userid AND content_id = :programid');
     $stmt->bindValue(':review', $nrev);
     $stmt->bindValue(':userid', $userid2);
     $stmt->bindValue(':programid', $programid2);
