5e8bbd18757c50b2dbbfd4c4174cbf67ddb3c2a6
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-15 17:06:39 -0500

    button design

--- web/project1/index.php
+++ web/project1/index.php
@@ -86,10 +86,13 @@ $serv = $stmt2->fetchAll(PDO::FETCH_ASSOC);
                 echo ">$servicename</option>";
             }
         ?>
-  </select>  
+  </select><br>  
        <?php
             if (isset ($_SESSION['userid'])){
-                echo"<a href='suggestion.php' class='btn btn-dark me-2' role='button'>Suggest New Content</a>";
+                echo"
+                <div class='d-grid gap-2'>
+                <a href='suggestion.php' class='btn btn-dark me-2' role='button'>Suggest New Content</a>
+                </div>";
             }
        ?>
   
