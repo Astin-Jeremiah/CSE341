7c972d2e887fd72bc9957fb4213890ba6b91be1c
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-15 22:58:22 -0500

    fix sql

--- web/project1/addtodb.php
+++ web/project1/addtodb.php
@@ -5,6 +5,8 @@ $name = htmlspecialchars($_POST['showname']);
 $about = htmlspecialchars($_POST['description']);
 $service = htmlspecialchars($_POST['service']);
 $link = htmlspecialchars($_POST['link']);
+$date = date("m/d/Y");
+
 
 $db = get_db();
 $stmt = $db->prepare('INSERT INTO content (content_name, description, picture, service_id) VALUES (:name, :description, :link, :service)');
@@ -13,10 +15,21 @@ $stmt->bindValue(':description', $about);
 $stmt->bindValue(':link', $link);  
 $stmt->bindValue(':service', $service);    
 $stmt->execute();
+
+$marksinked = markedsinked($date);
     
 $new_page = "index.php";    
 header("Location: $new_page");
     
-die();;
+die();
+
+
+function markedsinked($date) {
+$db = get_db();
+$stmt2 = $db->prepare('INSERT INTO suggested_content (sinked) VALUES (:date)');
+$stmt2->bindValue(':date', $date);   
+$stmt2->execute();
+    
+}
 
 ?>
