a03b9c1a3e6b1e1cd6fed6ce153f6598757baec0
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-19 14:40:11 -0500

    try

--- web/project1/programdetails.php
+++ web/project1/programdetails.php
@@ -35,7 +35,7 @@ $pid = htmlspecialchars($_GET['id']);
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $details['content_name']; ?></title>
+    <title><?php echo $detail['content_name']; ?></title>
     <link href="../../bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="style.css" rel="stylesheet">
