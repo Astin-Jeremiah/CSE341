aea390b16db825c1dceb610fed6a325ef7212368
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-20 18:19:18 -0500

    update review process

--- web/project1/updatereview.php
+++ web/project1/updatereview.php
@@ -1,15 +1,15 @@
 <?php
 
-var_dump($_POST);
-
 $nrev = htmlspecialchars($_POST['newreview']);
 $userid2 = htmlspecialchars($_POST['aid']);
 $programid2 = htmlspecialchars($_POST['cid']);
 $result = htmlspecialchars($_POST['result']);
+$update = "Update Review";
+$delete = "Delete Review";
 
-echo $result;
+require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
 
-/*require $_SERVER['DOCUMENT_ROOT'] . '/modules/dbConnect.php';
+if ($result == $update){
 $db = get_db();
 $stmt = $db->prepare('UPDATE reviews SET note = :review WHERE account_id = :userid AND content_id = :programid');
 $stmt->bindValue(':review', $nrev);
@@ -19,5 +19,17 @@ $stmt->execute();
 
 $new_page = "accountinfo.php";    
 header("Location: $new_page");
-die();*/
+die();
+}
+
+if ($result == $delete){
+$db = get_db();
+$stmt = $db->prepare('DELETE FROM reviews WHERE account_id = :userid AND content_id = :programid');
+$stmt->bindValue(':userid', $userid2);
+$stmt->bindValue(':programid', $programid2);
+$stmt->execute();
+
+$new_page = "accountinfo.php";    
+header("Location: $new_page");
+die();  
 ?>
