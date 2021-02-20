43b43852ddd45453d2592f97c479a29868e51625
Astin-Jeremiah <43362521+Astin-Jeremiah@users.noreply.github.com>
2021-02-20 18:38:35 -0500

    add css hover

--- web/project1/accountinfo.php
+++ web/project1/accountinfo.php
@@ -103,7 +103,7 @@ $suggestion = $stmt4->fetchAll(PDO::FETCH_ASSOC);
                         $review = $rev['note'];
                         echo "
                         <form id='specialform2' action='editreview.php' method='POST'>
-                        <li><button type='submit' id='special' title='Edit Review' class='text-norwap'><b>$showname</b> - $review</button></li>
+                        <li class='hover'><button type='submit' id='special' title='Edit Review' class='text-norwap'><b>$showname</b> - $review</button></li>
                         <input type='hidden' id='accountid' name='accountid' value='$accid'>
                         <input type='hidden' id='contentid' name='contentid' value='$conid'>
                         <input type='hidden' id='contentid' name='review' value='$review'>
@@ -126,7 +126,7 @@ $suggestion = $stmt4->fetchAll(PDO::FETCH_ASSOC);
                             $serv = $sug['service_id'];
                             echo "
                             <form id='specialform' action='databaseinfo.php' method='POST'>
-                            <li><button type='submit' id='special' class='text-norwap' title='Add Suggestion To Database'><b> $sname</b> - $desc</button></li>
+                            <li class='hover'><button type='submit' id='special' class='text-norwap' title='Add Suggestion To Database'><b> $sname</b> - $desc</button></li>
                             <input type='hidden' id='showname' name='showname' value='$sname'>
                             <input type='hidden' id='description' name='description' value='$desc'>
                             <input type='hidden' id='service' name='service' value='$serv'>
